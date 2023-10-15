d1b6f1cfa91b76a9835d732b577b0c2ad2922c0e
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-15 14:05:22 +0530

    //

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ApolloProvider from './';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(ApolloProvider, document.getElementById('root'));
+
 
 
