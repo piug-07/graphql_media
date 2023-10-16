f89747292709a8e817d3fa15c510a321d3b00b93
Piyush Gupta <guptapiyush315@gmail.com>
2023-10-16 23:10:21 +0530

    .

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = new ApolloServer({
     app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('/*', function (req, res) {
-      res.sendFile(path.join(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join(path.resolve(), 'build', 'index.html'));
     });
 
 
