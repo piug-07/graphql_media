97b1a25614380d0bdd7c4d9da4a665aab207aa14
piyush gupta <guptapiyush315@gmail.com>
2023-10-17 00:11:17 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGODB:
-      'mongodb+srv://guptapiyush315:vYWZrmMtHp395W29@real-estate.esrb235.mongodb.net/?retryWrites=true&w=majority',
-    SECRET_KEY: 'graphqlPiyush'
-  };
-
-
-
